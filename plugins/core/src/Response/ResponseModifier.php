1556db7ed133d2b2ccea00cc996aba53ee460713
chris cnizzardini <cnizzardini@gmail.com>
2021-06-23 15:23:40 -0400

    Improves test coverage and comments for the ResponseModifier

--- plugins/core/src/Response/ResponseModifier.php
+++ plugins/core/src/Response/ResponseModifier.php
@@ -11,7 +11,7 @@ use Cake\Http\ServerRequest;
 class ResponseModifier
 {
     /**
-     * Type Alias
+     * Type Alias that will be used to modify the response
      *
      * @example `jsonld` or `hal+json`
      * @var string
@@ -19,7 +19,9 @@ class ResponseModifier
     private $type;
 
     /**
-     * MIME Type mapping array
+     * MIME Type mapping array, these are the mime type's associated with the type alias. For example, jsonld has a
+     * mime type mapping of ['application/ld+json']. Some type aliases can be associated with multiple mime types,
+     * which are requested via the HTTP Header `accepts`.
      *
      * @example `['application/ld+json']`
      * @var string[]
@@ -27,6 +29,8 @@ class ResponseModifier
     private $mimeTypes;
 
     /**
+     * The View Class associated with $this->type alias
+     *
      * @example MixerApi/JsonLdView.JsonLd
      * @var string
      */
@@ -45,7 +49,10 @@ class ResponseModifier
     }
 
     /**
-     * Registers Controller.startup listener
+     * Registers Controller.initialize listener which performs the following actions:
+     * - Modifies the CakePHP Response type map if necessary to add additional mime types (note: this will be removed
+     * in a future version of the application).
+     * - Sets the view class map to render responses in the requested mime type.
      *
      * @return void
      */
@@ -57,6 +64,7 @@ class ResponseModifier
                 /** @var \Cake\Controller\Controller $controller */
                 $controller = $event->getSubject();
 
+                // @codeCoverageIgnoreStart
                 if ((new Response())->getMimeType($this->type) === false) {
                     $response = $this->modify(
                         $controller->getRequest(),
@@ -64,6 +72,7 @@ class ResponseModifier
                     );
                     $controller->setResponse($response);
                 }
+                // @codeCoverageIgnoreEnd
 
                 if ($controller->components()->has('RequestHandler')) {
                     $controller->RequestHandler->setConfig(
@@ -75,19 +84,20 @@ class ResponseModifier
     }
 
     /**
-     * Appends JSON-LD MIME types to the Response if the ServerRequest accepts JSON-LD
+     * Adds unsupported mime types to the CakePHP response object. Note, as of CakePHP 4.2 hal+json, hal+xml, and
+     * ld+json are valid mimetypes. See https://github.com/cakephp/cakephp/issues/14796, this code can be removed
+     * in future versions of this application.
      *
+     * @todo remove when version 4.1 or lower is no longer supported
      * @param \Cake\Http\ServerRequest $request cake ServerRequest
      * @param \Cake\Http\Response $response cake Response
      * @return \Cake\Http\Response
      */
     public function modify(ServerRequest $request, Response $response): Response
     {
-        if (!$this->isRequestOfThisMimeType($request)) {
-            return $response;
+        if ($this->isRequestOfThisMimeType($request)) {
+            $response->setTypeMap($this->type, $this->mimeTypes);
         }
-
-        $response->setTypeMap($this->type, $this->mimeTypes);
 
         return $response;
     }
