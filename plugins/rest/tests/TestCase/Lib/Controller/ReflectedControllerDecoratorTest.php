a753a53e9060fd647d512fe554a0d70d3bcba014
chris cnizzardini <cnizzardini@gmail.com>
2021-06-24 23:54:01 -0400

    [Rest] increase coverage and switch to snake_case

--- plugins/rest/tests/TestCase/Lib/Controller/ReflectedControllerDecoratorTest.php
+++ plugins/rest/tests/TestCase/Lib/Controller/ReflectedControllerDecoratorTest.php
@@ -4,11 +4,12 @@ namespace MixerApi\Rest\Test\TestCase\Lib\Controller;
 
 use Cake\TestSuite\TestCase;
 use MixerApi\Rest\Lib\Controller\ReflectedControllerDecorator;
+use MixerApi\Rest\Lib\Exception\InvalidControllerException;
 use MixerApi\Rest\Lib\Exception\RunTimeException;
 
 class ReflectedControllerDecoratorTest extends TestCase
 {
-    public function testConstruct()
+    public function test_construct(): void
     {
         $decorator = new ReflectedControllerDecorator(
             'MixerApi\Rest\Test\App\Controller\ActorsController'
@@ -23,11 +24,15 @@ class ReflectedControllerDecoratorTest extends TestCase
         $this->assertInstanceOf(ReflectedControllerDecorator::class, $decorator);
     }
 
-    public function testConstructException()
+    public function test_construct_exception(): void
     {
         $this->expectException(RunTimeException::class);
-        $decorator = new ReflectedControllerDecorator(
-            'MixerApi\Rest\Test\Nope'
-        );
+        new ReflectedControllerDecorator('MixerApi\Rest\Test\Nope');
+    }
+
+    public function test_construct_invalid_controller_exception(): void
+    {
+        $this->expectException(InvalidControllerException::class);
+        new ReflectedControllerDecorator('Cake\TestSuite\TestCase');
     }
 }
