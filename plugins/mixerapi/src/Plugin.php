6c4d7175cbfbef609b7a4e64583b6618ade330c1
chris cnizzardini <cnizzardini@gmail.com>
2021-12-05 14:07:16 -0500

    remove experimental plugin

--- plugins/mixerapi/src/Plugin.php
+++ plugins/mixerapi/src/Plugin.php
@@ -46,7 +46,6 @@ class Plugin extends BasePlugin
     public function bootstrap(PluginApplicationInterface $app): void
     {
         $app->addPlugin('MixerApi/CollectionView');
-        $app->addPlugin('MixerApi/Crud');
         $app->addPlugin('MixerApi/ExceptionRender');
         $app->addPlugin('MixerApi/HalView');
         $app->addPlugin('MixerApi/JsonLdView');
