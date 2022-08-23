bc273263ecea39d95a453416e8faad5b4de42474
Chris Nizzardini <cnizzardini@gmail.com>
2022-08-23 10:52:16 -0400

    make search optional (#118)
    No longer load Search plugin by default.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -121,10 +121,15 @@ ini_set('intl.default_locale', Configure::read('App.defaultLocale'));
  * Register application error and exception handlers.
  */
 $isCli = PHP_SAPI === 'cli';
-if ($isCli) {
-    (new ConsoleErrorHandler(Configure::read('Error')))->register();
+if (str_starts_with(Configure::version(), '4.4')) {
+    (new \Cake\Error\ErrorTrap(Configure::read('Error')))->register();
+    (new \Cake\Error\ExceptionTrap(Configure::read('Error')))->register();
 } else {
-    (new ErrorHandler(Configure::read('Error')))->register();
+    if ($isCli) {
+        (new ConsoleErrorHandler(Configure::read('Error')))->register();
+    } else {
+        (new ErrorHandler(Configure::read('Error')))->register();
+    }
 }
 
 /*
