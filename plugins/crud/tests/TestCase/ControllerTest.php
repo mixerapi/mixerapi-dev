bb554c72f4a43ab30084a988c03fd43e200499fc
Chris Nizzardini <chris.nizzardini@thecmigroupa.ca>
2024-01-06 13:15:41 -0500

    remove debug code

--- plugins/crud/tests/TestCase/ControllerTest.php
+++ plugins/crud/tests/TestCase/ControllerTest.php
@@ -45,7 +45,6 @@ class ControllerTest extends TestCase
 
     public function test_add(): void
     {
-        $this->disableErrorHandlerMiddleware();
         $this->post('/actors.json', [
             'last_name' => 'ever',
             'first_name' => 'greatest'
