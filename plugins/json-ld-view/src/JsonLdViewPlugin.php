a4dcde1c9377309ba584c70da75ac46f357851b0
Chris Nizzardini <cnizzardini@gmail.com>
2026-02-15 12:37:11 -0500

    Handle cakephp 5.3 deprecations (#160)
    Updates plugin entrypoint class names across the MixerApi plugin suite to address CakePHP 5.3 deprecation warnings and aligns CI to test newer runtime/framework ranges.

--- plugins/json-ld-view/src/JsonLdViewPlugin.php
+++ plugins/json-ld-view/src/JsonLdViewPlugin.php
@@ -1,19 +1,20 @@
 <?php
 declare(strict_types=1);
 
-namespace MixerApi\HalView;
+namespace MixerApi\JsonLdView;
 
 use Cake\Core\BasePlugin;
+use Cake\Core\Configure;
 use Cake\Core\PluginApplicationInterface;
 
-class Plugin extends BasePlugin
+class JsonLdViewPlugin extends BasePlugin
 {
     /**
      * Plugin name.
      *
-     * @var string|null
+     * @var string
      */
-    protected ?string $name = 'MixerApi/HalView';
+    protected ?string $name = 'MixerApi/JsonLdView';
 
     /**
      * Console middleware
@@ -50,5 +51,21 @@ class Plugin extends BasePlugin
     public function bootstrap(PluginApplicationInterface $app): void
     {
         parent::bootstrap($app);
+
+        try {
+            Configure::load('jsonld_config');
+        } catch (\Exception $e) {
+        }
+
+        if (Configure::read('JsonLdView') === null) {
+            Configure::write('JsonLdView', [
+                'isHydra' => false,
+                'schemaUrl' => 'http://schema.org',
+                'vocabUrl' => '/vocab',
+                'title' => 'API Documentation',
+                'description' => '',
+                'entrypointUrl' => '/',
+            ]);
+        }
     }
 }
---
+++ plugins/jwt-auth/src/JwtAuthPlugin.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace MixerApi\JwtAuth;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin for JwtAuth
+ */
+class JwtAuthPlugin extends BasePlugin
+{
+    /**
+     * Plugin name.
+     *
+     * @var string
+     */
+    protected ?string $name = 'MixerApi/JwtAuth';
+
+    /**
+     * Do bootstrapping or not
+     *
+     * @var bool
+     */
+    protected bool $bootstrapEnabled = false;
+
+    /**
+     * Console middleware
+     *
+     * @var bool
+     */
+    protected bool $consoleEnabled = false;
+
+    /**
+     * Enable middleware
+     *
+     * @var bool
+     */
+    protected bool $middlewareEnabled = false;
+
+    /**
+     * Register container services
+     *
+     * @var bool
+     */
+    protected bool $servicesEnabled = false;
+
+    /**
+     * Load routes or not
+     *
+     * @var bool
+     */
+    protected bool $routesEnabled = false;
+}
